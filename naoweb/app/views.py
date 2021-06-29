371fa12a0bb313b6f16cd806232a25bdcb2cbfc2
andris.zacs <andris.zacs@accenture.com>
2021-06-29 19:26:20 +0300

    initial

---
+++ naoweb/app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+import datetime
+from django.http import HttpResponse
+
+async def current_datetime(request):
+    now = datetime.datetime.now()
+    html = '<html><body>It is now %s.</body></html>' % now
+    return HttpResponse(html)
+
